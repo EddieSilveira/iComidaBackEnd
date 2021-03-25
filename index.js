5a2884fda3682126e660efd0a61a88b04da9ff73
EddieSilveira <eddie_aparecido@hotmail.com>
2021-03-24 22:14:56 -0300

    Add routes config e http para testes

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+require('dotenv').config(); // Carrega as "variáveis de ambiente"
+const InicializaMongoServer = require('./config/Db');
+//Definindo as rotas da aplicação
+const rotasCategoria = require('./routes/Categoria');
+
+//Inicializamos o servidor MongoDB
+InicializaMongoServer();
+const app = express();
+//Porta Default
+const PORT = process.env.PORT;
+//Parse conteúdo JSON
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  const idiomas = req.headers['accept-language'];
+  res.json({
+    mensagem: 'API iComida 100% funcional! 👏',
+    versao: '1.0.1',
+  });
+});
+/* Rotas da Categoria */
+app.use('/categorias', rotasCategoria);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Servidor Web iniciado na porta ${PORT}`);
+});
